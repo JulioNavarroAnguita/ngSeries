85b7fbe269abcde37d9faf1784877fb86f95aefb
JulioNavarroAnguita <minipelopi@hotmail.com>
2019-02-25 09:29:51 +0100

    Continuando el proyecto, con las rutas getSeries y getSerie.

--- src/app/series.service.ts
+++ src/app/series.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Serie } from './modelos/serie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeriesService {
 
-  constructor() { }
+  url = 'http://localhost:3000/series';
+
+  constructor(private http: HttpClient) { }
+
+  getSeries(): Observable<Serie[]> {
+    return this.http.get<Serie[]>(this.url);
+  }
+
+  getSerie(id: number | string): Observable<Serie> {
+    return this.http.get<Serie>(this.url + '/' + id);
+  }
+
 }
